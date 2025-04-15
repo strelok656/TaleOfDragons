69ea0535ecea74bda8e8247bad6ea0f853bc1b2c
strelok656 <almazgallyamov10@mail.ru>
2025-04-15 21:12:44 +0500

    Added Honey Beer

---
+++ Content/Effects/Honeymoon.cs
@@ -0,0 +1,22 @@
+﻿using Terraria;
+using Terraria.Localization;
+using Terraria.ModLoader;
+
+namespace TaleOfDragons.Content.Effects
+{
+    internal class Honeymoon : ModBuff
+    {
+        public static readonly int defenseBonus = 5;
+        public static readonly int damageBonus = 15;
+        public static readonly int healthReduction = 30;
+
+        public override LocalizedText Description => base.Description.WithFormatArgs(damageBonus, defenseBonus, healthReduction);
+
+        public override void Update(Player player, ref int buffIndex)
+        {
+            player.statDefense += defenseBonus;
+            player.GetDamage<GenericDamageClass>() += damageBonus;
+            player.statLifeMax2 -= healthReduction;
+        }
+    }
+}
---
+++ Content/Items/Consumables/HoneyBeer.cs
@@ -0,0 +1,36 @@
+﻿using TaleOfDragons.Content.Effects;
+using Terraria;
+using Terraria.ID;
+using Terraria.Localization;
+using Terraria.ModLoader;
+
+namespace TaleOfDragons.Content.Items.Consumables
+{
+    class HoneyBeer : ModItem
+    {
+        public static readonly int defenseBonus = 5;
+        public static readonly int damageBonus = 15;
+        public static readonly int healthReduction = 30;
+
+        public override LocalizedText Tooltip => base.Tooltip.WithFormatArgs(damageBonus, defenseBonus, healthReduction);
+
+        public override void SetStaticDefaults()
+        {
+            Item.ResearchUnlockCount = 10;
+        }
+
+        public override void SetDefaults()
+        {
+            Item.height = Item.width = 15;
+            Item.scale = 0.8f;
+            Item.value = 100;
+            Item.useStyle = ItemUseStyleID.DrinkLiquid;
+            Item.UseSound = SoundID.Item3;
+            Item.useTime = Item.useAnimation = 15;
+            Item.useTurn = true;
+            Item.maxStack = Item.CommonMaxStack;
+            Item.buffType = ModContent.BuffType<Honeymoon>();
+            Item.buffTime = 18300;
+        }
+    }
+}
